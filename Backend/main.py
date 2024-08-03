5244cbe70b578d90569509380f90655a5b80269a
TpSoftDev <tp.softwaredev@gmail.com>
2024-08-03 13:02:05 -0700

    Buy Signal for Crypto

--- Backend/main.py
+++ Backend/main.py
@@ -5,24 +5,30 @@ def main():
     mt.initialize()
     mt.login(105098537, 'Cornw@ll246!', 'Trading.comMarkets-MT5')
 
-    symbol = "EURUSD"
-    lot = 1.0
+    # get all symbols
+    symbols = mt.symbols_get()
+    print('Symbols: ', symbols)
 
 
-    request = {
-        "action": mt.TRADE_ACTION_DEAL,
-        "symbol": symbol,
-        "volume": lot,
-        "type": mt.ORDER_TYPE_BUY,
-        "price": mt.symbol_info_tick(symbol).ask,
-        "sl" : 1.06695,
-        "tp" : 1.09695,
-        "comment" : 'Pyhon Script Buy',
-        
-    }
+    #symbol = "BTC/USD"
+    #lot = 0.1
+
+
+    #request = {
+    #    "action": mt.TRADE_ACTION_DEAL,
+    #    "symbol": symbol,
+    #   "volume": lot,
+    #    "type": mt.ORDER_TYPE_BUY,
+    #    "price": mt.symbol_info_tick(symbol).ask,
+    #    "sl": 1.06695,
+    #    "tp": 1.09695,
+    #    "comment": 'Pyhon Script Buy',
+    #    "type_time": mt.ORDER_TIME_GTC,
+    #    "type_filling": mt.ORDER_FILLING_IOC
+    #}
 
     # send a trading request
-    result = mt5.order_send(request)
+    #result = mt.order_send(request)
 
 
 
